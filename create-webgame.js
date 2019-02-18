2a9c7acb31c202a74b547a4331dd5d301d67ce12
Michael Clayton <mclayton@redhat.com>
2019-02-18 15:15:27 -0500

    add test output

---
+++ create-webgame.js
@@ -0,0 +1,5 @@
+#!/usr/bin/env node
+
+console.log('create-webgame is here');
+
+// yo --no-insight --no-update-notifier webgame
--- create-webgame.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env node
-
-npx yo --no-insight --no-update-notifier webgame
